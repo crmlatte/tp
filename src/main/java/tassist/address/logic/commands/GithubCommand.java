9064dececf8c296d075675c06f4d47dc06bbb1bc
Sulaksha <>
2025-03-13 00:00:00 +0800

    Add StudentId class

--- src/main/java/tassist/address/logic/commands/GithubCommand.java
+++ src/main/java/tassist/address/logic/commands/GithubCommand.java
@@ -2,6 +2,8 @@ package tassist.address.logic.commands;
 
 import tassist.address.logic.commands.exceptions.CommandException;
 import tassist.address.model.Model;
+import tassist.address.model.person.Github;
+import tassist.address.model.person.StudentId;
 
 /**
  * Changes the github of an existing person in the address book.
@@ -9,7 +11,6 @@ import tassist.address.model.Model;
 public class GithubCommand extends Command {
 
     public static final String COMMAND_WORD = "github";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Edits the github of the person identified "
             + "by the {Student id number}. "
@@ -19,12 +20,35 @@ public class GithubCommand extends Command {
             + "Example: " + COMMAND_WORD + " A1234567X "
             + "g/https://github.com/tammzz";
 
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
-            "Github command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "Student id: %1$d, Github: %2$s";
+    private final StudentId studentId;
+    private final Github github;
+
+    public GithubCommand(StudentId studentId, Github github) {
+        this.studentId = studentId;
+        this.github = github;
+    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(
+                String.format(MESSAGE_ARGUMENTS, studentId, github));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GithubCommand)) {
+            return false;
+        }
+
+        GithubCommand e = (GithubCommand) other;
+        return studentId.equals(e.studentId)
+                && github.equals(e.github);
     }
 
 }
