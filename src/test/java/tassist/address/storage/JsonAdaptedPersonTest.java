715f35dabf014c728b617b020b09d61f723c98a0
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-03-13 21:45:47 +0800

    Fix checkstyle

--- src/test/java/tassist/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/tassist/address/storage/JsonAdaptedPersonTest.java
@@ -93,7 +93,8 @@ public class JsonAdaptedPersonTest {
     @Test
     public void toModelType_invalidAddress_throwsIllegalValueException() {
         JsonAdaptedPerson person =
-                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, INVALID_ADDRESS, VALID_STUDENTID, VALID_TAGS);
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, INVALID_ADDRESS, VALID_STUDENTID,
+                        VALID_TAGS);
         String expectedMessage = Address.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
