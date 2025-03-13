e151558c565e57428fe4620712d1a191f8b42878
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-03-13 19:25:08 +0800

    Update StudentId class

--- src/main/java/tassist/address/model/person/StudentId.java
+++ src/main/java/tassist/address/model/person/StudentId.java
@@ -1,4 +1,21 @@
 package tassist.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+import static tassist.address.commons.util.AppUtil.checkArgument;
+
 public class StudentId {
+
+    public static final String MESSAGE_CONSTRAINTS = "Message has not been instantiated";
+
+    public final String value;
+
+    public StudentId(String email) {
+        requireNonNull(email);
+        checkArgument(isValidStudentId(email), MESSAGE_CONSTRAINTS);
+        value = email;
+    }
+
+    public static boolean isValidStudentId(String test) {
+        return true; //implementation to be done later
+    }
 }
