fbff20389d592519872bbd44f8c2d4e57d714c17
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-03-15 01:21:35 +0800

    Improve MESSAGE_CONSTRAINT

--- src/main/java/tassist/address/model/person/StudentId.java
+++ src/main/java/tassist/address/model/person/StudentId.java
@@ -9,9 +9,12 @@ import static tassist.address.commons.util.AppUtil.checkArgument;
  */
 public class StudentId {
 
-    public static final String MESSAGE_CONSTRAINTS = "Must be in the format of AXXXXXXXN, where X is an integer and"
-            + "\nN is any letter from the Alphbet (eg. A to Z)."
-            + "\nEnsure that both letters A and N are capitalised.";
+    public static final String MESSAGE_CONSTRAINTS =
+            "The Student ID must follow the format AXXXXXXXN, where:\n" +
+                    "- A is the uppercase letter 'A'.\n" +
+                    "- X represents seven digits (0-9).\n" +
+                    "- N is any uppercase letter from A to Z.\n" +
+                    "Both 'A' and 'N' must be capitalized.";
     public static final String VALIDATION_REGEX =  "^A\\d{7}[A-Z]$";
 
     public final String value;
