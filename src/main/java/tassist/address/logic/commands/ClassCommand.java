0fc727189adb98df22dc1c67f5a178e264c0de5a
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-03-17 01:56:32 +0800

    Add tests for ClassCommand.execute()

--- src/main/java/tassist/address/logic/commands/ClassCommand.java
+++ src/main/java/tassist/address/logic/commands/ClassCommand.java
@@ -30,10 +30,8 @@ public class ClassCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_CLASS + " T01";
 
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Class Number: %2$s";
-
-    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Assigned tutorial class to student: %1$s";
-    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed tutorial class from student: %1$s";
+    public static final String MESSAGE_ADD_CLASS_SUCCESS = "Assigned tutorial class to student: %1$s";
+    public static final String MESSAGE_DELETE_CLASS_SUCCESS = "Removed tutorial class from student: %1$s";
 
     private final Index index;
     private final ClassNumber classNumber;
@@ -76,7 +74,7 @@ public class ClassCommand extends Command {
      * {@code personToEdit}.
      */
     private String generateSuccessMessage(Person personToEdit) {
-        String message = !classNumber.value.isEmpty() ? MESSAGE_ADD_REMARK_SUCCESS : MESSAGE_DELETE_REMARK_SUCCESS;
+        String message = !classNumber.value.isEmpty() ? MESSAGE_ADD_CLASS_SUCCESS : MESSAGE_DELETE_CLASS_SUCCESS;
         return String.format(message, Messages.format(personToEdit));
     }
 
--- src/test/java/tassist/address/logic/commands/ClassCommandTest.java
+++ src/test/java/tassist/address/logic/commands/ClassCommandTest.java
@@ -1,35 +1,70 @@
 package tassist.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static tassist.address.logic.commands.ClassCommand.MESSAGE_ARGUMENTS;
 import static tassist.address.logic.commands.CommandTestUtil.VALID_CLASS_AMY;
 import static tassist.address.logic.commands.CommandTestUtil.VALID_CLASS_BOB;
 import static tassist.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static tassist.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static tassist.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static tassist.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static tassist.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import tassist.address.commons.core.index.Index;
+import tassist.address.logic.Messages;
+import tassist.address.logic.commands.exceptions.CommandException;
 import tassist.address.model.Model;
 import tassist.address.model.ModelManager;
 import tassist.address.model.UserPrefs;
 import tassist.address.model.person.ClassNumber;
+import tassist.address.model.person.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ClassCommand.
  */
 public class ClassCommandTest {
 
+    private static final ClassNumber VALID_CLASS = new ClassNumber("T01");
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute() {
-        final ClassNumber classNumber = new ClassNumber("T01");
+    public void execute_validClassNumber_success() throws CommandException {
+        ClassCommand classCommand = new ClassCommand(INDEX_FIRST_PERSON, VALID_CLASS);
 
-        assertCommandFailure(new ClassCommand(INDEX_FIRST_PERSON, classNumber), model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), classNumber));
+        ClassNumber validClassNumber = new ClassNumber("T01");
+        CommandResult result = classCommand.execute(model);
+
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        assertEquals(validClassNumber, editedPerson.getClassNumber()); // Assert tutorial was correctly assigned
+        assertEquals(String.format(ClassCommand.MESSAGE_ADD_CLASS_SUCCESS, Messages.format(editedPerson)),
+                result.getFeedbackToUser()); // Assert success message
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ClassCommand remarkCommand = new ClassCommand(outOfBoundIndex, new ClassNumber(VALID_CLASS_BOB));
+
+        assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        ClassCommand remarkCommand = new ClassCommand(outOfBoundIndex, new ClassNumber(VALID_CLASS_BOB));
+
+        assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
