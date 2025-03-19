38a0dc4745b158cc642c401c1cf3f88a3b7b503d
Sulaksha <>
2025-03-19 15:38:45 +0800

    Modify Valid_studentId_Bob

--- src/test/java/tassist/address/logic/commands/CommandTestUtil.java
+++ src/test/java/tassist/address/logic/commands/CommandTestUtil.java
@@ -37,7 +37,7 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_STUDENTID_AMY = "A0000000B";
-    public static final String VALID_STUDENTID_BOB = "A0000000B";
+    public static final String VALID_STUDENTID_BOB = "A0000000D";
     public static final String VALID_GITHUB_AMY = "https://github.com/amy123";
     public static final String VALID_GITHUB_BOB = "https://github.com/bobb-33";
     public static final String VALID_TAG_HUSBAND = "husband";
