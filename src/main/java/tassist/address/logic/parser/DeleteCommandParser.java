7a040ac1f1091c7db004e6c5beb6d3b3883063b0
Sulaksha <>
2025-03-25 02:49:06 +0800

    Update DeleteCommand parser

--- src/main/java/tassist/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/tassist/address/logic/parser/DeleteCommandParser.java
@@ -1,6 +1,8 @@
 package tassist.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static tassist.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static tassist.address.model.person.StudentId.VALIDATION_REGEX;
 
 import tassist.address.commons.core.index.Index;
 import tassist.address.commons.exceptions.IllegalValueException;
@@ -19,18 +21,30 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
-        } catch (ParseException pe) {
+        requireNonNull(args);
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args);
+        String trimmedArgs = argumentMultimap.getPreamble().trim();
+
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteCommand.MESSAGE_USAGE));
+        }
+        if (trimmedArgs.matches(VALIDATION_REGEX)) {
             try {
-                StudentId studentId = ParserUtil.parseStudentId(args);
+                StudentId studentId = ParserUtil.parseStudentId(trimmedArgs);
                 return new DeleteCommand(studentId);
             } catch (IllegalValueException ive) {
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                        DeleteCommand.MESSAGE_USAGE), pe);
+                        DeleteCommand.MESSAGE_USAGE), ive);
             }
         }
-    }
 
+        try {
+            Index index = ParserUtil.parseIndex(trimmedArgs);
+            return new DeleteCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteCommand.MESSAGE_USAGE), ive);
+        }
+    }
 }
