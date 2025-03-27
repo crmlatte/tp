ff42e672db728508408184b085f1fd0f15018be0
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-03-27 20:50:48 +0800

    Add tests for ImportCommandParser

--- src/test/java/tassist/address/logic/parser/ImportCommandParserTest.java
+++ src/test/java/tassist/address/logic/parser/ImportCommandParserTest.java
@@ -1,4 +1,36 @@
 package tassist.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import tassist.address.logic.commands.ImportCommand;
+
 public class ImportCommandParserTest {
+    private ImportCommandParser parser = new ImportCommandParser();
+
+    @Test
+    public void parse_validAbsolutePath_success() {
+        String input = "import fp/ /stub/path/to/sample.csv";
+        Path expectedPath = Paths.get("/stub/path/to/sample.csv").toAbsolutePath();
+        ImportCommand expectedCommand = new ImportCommand(expectedPath);
+
+        ImportCommand command = parser.parse(input);
+
+        assertEquals(expectedCommand, command);
+    }
+
+    @Test
+    public void parse_validRelativePath_success() {
+        String input = "import fp/ data/sample.csv";
+        Path expectedPath = Paths.get("data/sample.csv").toAbsolutePath();
+        ImportCommand expectedCommand = new ImportCommand(expectedPath);
+
+        ImportCommand command = parser.parse(input);
+
+        assertEquals(expectedCommand, command);
+    }
 }
