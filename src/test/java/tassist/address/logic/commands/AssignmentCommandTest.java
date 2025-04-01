d2c7d79f7b44511df0f99e75cd5124e207868ad0
Casielim <e1300513@u.nus.edu>
2025-04-01 18:03:53 +0800

    Disallow assigning overdue timed events

--- src/test/java/tassist/address/logic/commands/AssignmentCommandTest.java
+++ src/test/java/tassist/address/logic/commands/AssignmentCommandTest.java
@@ -2,19 +2,22 @@ package tassist.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tassist.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static tassist.address.testutil.Assert.assertThrows;
 
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 
+import tassist.address.commons.core.index.Index;
+import tassist.address.logic.commands.exceptions.CommandException;
 import tassist.address.model.Model;
 import tassist.address.model.ModelManager;
 import tassist.address.model.UserPrefs;
 import tassist.address.model.timedevents.Assignment;
 import tassist.address.testutil.TypicalAssignments;
+import tassist.address.testutil.TypicalPersons;
 
 public class AssignmentCommandTest {
 
@@ -74,5 +77,18 @@ public class AssignmentCommandTest {
                 "Complete the different quiz",
                 LocalDateTime.now().plusDays(7));
         assertFalse(assignmentCommand.equals(new AssignmentCommand(differentAssignment)));
+    }
+
+    @Test
+    public void execute_assignOverdueAssignment_throwsCommandException() {
+        Assignment overdueEvent = new Assignment("Quiz", "", LocalDateTime.now().minusDays(2));
+        model.addTimedEvent(overdueEvent);
+        model.addPerson(TypicalPersons.AMY);
+
+        AssignCommand command = new AssignCommand(Index.fromOneBased(3), Index.fromOneBased(1));
+
+        String expectedMessage = String.format(AssignCommand.MESSAGE_ASSIGN_FAILED_OVERDUE_ASSIGNMENT, "Quiz");
+        CommandException thrown = assertThrows(CommandException.class, () -> command.execute(model));
+        assertEquals(expectedMessage, thrown.getMessage());
     }
 }
