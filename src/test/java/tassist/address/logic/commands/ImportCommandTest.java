0519353aa5032ea0e68ea6f8a13b47a3c2ee832a
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-04-03 13:32:27 +0800

    Fix import errors

--- src/test/java/tassist/address/logic/commands/ImportCommandTest.java
+++ src/test/java/tassist/address/logic/commands/ImportCommandTest.java
@@ -1,7 +1,6 @@
 package tassist.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
