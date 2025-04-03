4ca37190b45c413f5eeb8e58d28b45fb260a36e8
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-04-03 17:38:02 +0800

    Update Github.MESSAGE_CONSTRAINTS messages

--- src/main/java/tassist/address/model/person/Github.java
+++ src/main/java/tassist/address/model/person/Github.java
@@ -11,15 +11,16 @@ public class Github {
     public static final String NO_GITHUB = "No Github assigned";
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Githubs links should be of the format https://github.com/{username} in the form [github URL]/username"
-            + "or 'No Github assigned' "
-            + "and adhere to the following constraints:\n"
-            + "1. The github url should be in the format ' https://github.com '.\n"
+            "GitHub links should be in the format: https://github.com/USERNAME\n"
+            + "Alternatively, you may leave the Github field blank to remove the GitHub link from a student.\n\n"
+            + "The URL must follow these rules:\n"
+            + "1. The base Github url should be in the format 'https://github.com'.\n"
             + "2. This is followed by a '/' and then a username."
             + "The username must:\n"
-            + "    - be 1 to 39 characters long\n"
-            + "    - consist of alphanumeric characters, separated only by dashes (-), if any.\n"
-            + "    - start and end with alphanumeric characters.\n"
+            + "    - Be 1 to 39 characters long\n"
+            + "    - Consist of alphanumeric characters.\n"
+            + "    - Separated only by dashes (-), if any.\n"
+            + "    - Start and end with alphanumeric characters.\n"
             + "Example:\n"
             + "https://github.com/JohnnyDoe";
 
