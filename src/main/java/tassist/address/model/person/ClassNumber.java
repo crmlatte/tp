cd2ed0ad6c58a383f79dafe31c29d5a53fb1477b
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-04-08 01:58:20 +0800

    Update UG

--- src/main/java/tassist/address/model/person/ClassNumber.java
+++ src/main/java/tassist/address/model/person/ClassNumber.java
@@ -14,7 +14,7 @@ public class ClassNumber {
             + "'T', 'R' and 'L' must be capitalized."
             + "Additionally, class number could be left blank to unassign a class"
             + "If you choose to assign 'Lxx' (where xx is 01-99), you can optionally add "
-            + "a capital letter suffix (e.g., 'L01A').";
+            + "a capital letter suffix (e.g. L01A).";
     public static final String VALIDATION_REGEX = "^(T|R)(0[1-9]|[1-9][0-9])$|^(L)(0[1-9]|[1-9][0-9])([A-Z])?";
     public static final String DEFAULT_CLASS = "No tutorial assigned";
     public final String value;
